26ae5c21a23e0c79d02cded2560d68cf5e4406aa
P4R4D0X15 <nicolas@warriordudimanche.net>
2025-12-17 22:51:56 +0100

    Fix to ValiditeNaive. Now naive and negation are coherent

--- Predicats/ValiditeNaive.ml
+++ Predicats/ValiditeNaive.ml
@@ -5,22 +5,7 @@ open Proposition.Formule
 (** all_prem_sublist : Construit la liste des Predicate.Set correspondante à
     toutes les sous listes de ats *)
 let all_prem_sublist (ats : string list) : Predicate_set.t list =
-  List.map
-    (fun at ->
-      List.fold_left
-        (fun acc a -> Predicate_set.add a acc)
-        Predicate_set.empty at)
-    (all_sublists ats)
-
-(** undef d : supprime dans pl toutes les zones qui sont définies dans d *)
-let undef (d : diagramme) (pl : Predicate_set.t list) : Predicate_set.t list =
-  let rec aux acc pl =
-    match pl with
-    | [] -> acc
-    | p :: q when Diag.mem p d -> aux acc q
-    | p :: q -> aux (p :: acc) q
-  in
-  aux [] pl
+  List.map Predicate_set.of_list (all_sublists ats)
 
 (** diag_from_undef d p : construit le liste des extensions possible du
     diagramme d sur une zone p*)
@@ -33,9 +18,11 @@ let diag_from_undef (d : diagramme) (p : Predicate_set.t) : diagramme list =
     zone A pourrait être complété en considérant les zones définies par une
     liste d'atomes [A; B; C]. *)
 let complete_diags (d : diagramme) (ats : string list) : diagramme list =
-  let ud = undef d (all_prem_sublist ats) in
+  let ud = List.filter (fun reg -> not (Diag.mem reg d)) (all_prem_sublist ats) in
   let d_list = List.map (fun p -> diag_from_undef d p) ud in
-  List.fold_left
+  match d_list with 
+  | [] -> [d]
+  | _ -> List.fold_left
     (fun acc dl -> conj_diag_list acc dl)
     (List.hd d_list) (List.tl d_list)
 
@@ -43,32 +30,44 @@ let complete_diags (d : diagramme) (ats : string list) : diagramme list =
     contradiction, c'est-à-dire s'il existe une zone non-vide de d1 qui est vide
     dans d2 ou inversement *)
 let is_contradiction (d1 : diagramme) (d2 : diagramme) : bool =
-  Diag.exists (fun pre v -> Diag.find pre d2 <> v) d1
-
-(** est_valid_premiss_conc b1 b2 teste si pour deux combinaisons booléennes de
-    formules pour syllogismes b1 et b2, b1 valide b2*)
-let est_valid_premiss_conc (b1 : boolCombSyllogismes) (b2 : boolCombSyllogismes)
-    : bool =
-  let db1 =
-    List.concat
-      (List.map (fun d -> complete_diags d []) (diags_of_bool_comb [] b1))
-  and db2 =
-    List.concat
-      (List.map (fun d -> complete_diags d []) (diags_of_bool_comb [] b2))
+  let check_zone pre v1 =
+    try
+      let v2 = Diag.find pre d2 in
+      v1 <> v2
+    with Not_found ->
+      false
   in
-  not (List.exists(fun d2 -> List.for_all (fun d1 -> is_contradiction d1 d2) db2) db1)
+  let contradiction_d1_to_d2 = Diag.exists check_zone d1 in
+  let check_zone_inverse pre v2 =
+    try
+      let v1 = Diag.find pre d1 in
+      v1 <> v2
+    with Not_found ->
+      false
+  in
+  let contradiction_d2_to_d1 = Diag.exists check_zone_inverse d2 in
+  contradiction_d1_to_d2 || contradiction_d2_to_d1
 
 (** temoins_invalidite_premisses_conc b1 b2 renvoie les diagrammes de la
     combinaison des prémisses b1 invalidant la conclusion b2 *)
 let temoins_invalidite_premisses_conc (b1 : boolCombSyllogismes)
     (b2 : boolCombSyllogismes) : diagramme list =
+
+  let atomes_b1 = atomes_of_bool_comb b1 and
+  atomes_b2 = atomes_of_bool_comb b2 in
+  let atomes =
+  List.sort_uniq String.compare (atomes_b1 @ atomes_b2) in
   let db1 =
-    List.concat
-      (List.map (fun d -> complete_diags d []) (diags_of_bool_comb [] b1))
+      (List.concat_map (fun d -> complete_diags d atomes) (diags_of_bool_comb [] b1))
   and db2 =
-    List.concat
-      (List.map (fun d -> complete_diags d []) (diags_of_bool_comb [] b2))
+      (List.concat_map (fun d -> complete_diags d atomes) (diags_of_bool_comb [] b2))
   in
-  List.find_all
+  List.filter
     (fun d1 -> List.for_all (fun d2 -> is_contradiction d1 d2) db2)
     db1
+
+(** est_valid_premiss_conc b1 b2 teste si pour deux combinaisons booléennes de
+    formules pour syllogismes b1 et b2, b1 valide b2*)
+let est_valid_premiss_conc (b1 : boolCombSyllogismes) (b2 : boolCombSyllogismes)
+    : bool =
+  (temoins_invalidite_premisses_conc b1 b2) = []
